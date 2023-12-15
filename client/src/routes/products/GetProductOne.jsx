223c4bac9d28490529851874e260ebe11ba797a1
gurureddy30 <153658709+gurureddy30@users.noreply.github.com>
2023-12-15 12:08:04 +0530

    Add files via upload

---
+++ client/src/routes/products/GetProductOne.jsx
@@ -0,0 +1,125 @@
+// Project by NISHANT CHHILLAR
+import React, { useEffect, useState } from "react";
+import "./GetProductOne.css";
+import { Divider } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+function GetProductOne() {
+    const { id } = useParams("");
+
+    const [IndividualData, setIndividualData] = useState([]);
+
+    const getIndividualData = async () => {
+        const res = await fetch(`http://localhost:8080/getproductsone/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const data = await res.json();
+
+        if (res.status !== 201) {
+            console.log("no data available");
+        } else {
+            console.log("get data");
+            setIndividualData(data);
+        }
+    };
+
+    useEffect(() => {
+        getIndividualData();
+    }, [id]);
+
+    const addtocart = async (id) => {
+        const check = await fetch(`http://localhost:8080/addcart/${id}`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                IndividualData
+            }),
+            // credentials:"include"
+        });
+
+        const data1 = await check.json();
+        console.log(data1 + "frontend data");
+
+        if (check.status !== 201) {
+            console.log("user invalid");
+            alert("user invalid");
+        } else {
+            alert("data added in your cart");
+        }
+    };
+
+    return (
+        <div className="get_product_one">
+            {IndividualData && Object.keys(IndividualData).length && (
+                <div className="get_products_section">
+                    <div className="left_product">
+                        <div className="image_container">
+                            <img
+                                src={IndividualData.url}
+                                alt={IndividualData.description}
+                                height={400}
+                            />
+                        </div>
+                        <Divider className="divider" />
+                        <div className="seller_description">
+                            <p>{IndividualData.description}</p>
+                        </div>
+                    </div>
+                    <div className="right_product">
+                        <div className="right_card">
+                            <h1>{IndividualData.title.longTitle}</h1>
+                            <div className="get_product_price">
+                                <div className="get_flex">
+                                    <span className="discount">
+                                        {IndividualData.price.discount}
+                                    </span>
+                                    <sup>₹</sup>
+                                    <h2>{IndividualData.price.cost}</h2>
+                                </div>
+                                <div className="get_flex grey">
+                                    <span>M.R.P.: </span>
+                                    <p className="mrp">
+                                        {IndividualData.price.mrp}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="offers">
+                                FREE delivery if ordered in 2 days.
+                            </div>
+                            <p className="in_stock">In stock</p>
+                            <div className="buttons">
+                                <a
+                                    className="add_to_cart"
+                                    onClick={() => addtocart(IndividualData.id)}
+                                >
+                                    Add to cart
+                                </a>
+                                <a className="buy_now">Buy now</a>
+                            </div>
+                            <b>Special offer: {IndividualData.discount}</b>
+                            <Divider className="divider" />
+                            <div className="product_description">
+                                <b>About this item</b>
+                                <ul>
+                                    <li>{IndividualData.about.feature1}</li>
+                                    <li>{IndividualData.about.feature2}</li>
+                                    <li>{IndividualData.about.feature3}</li>
+                                    <li>{IndividualData.about.feature4}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default GetProductOne;
