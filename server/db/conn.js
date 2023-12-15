60d3036aa7062e308691b3482a84b1ebd69bb16d
gurureddy30 <153658709+gurureddy30@users.noreply.github.com>
2023-12-15 12:13:57 +0530

    Add files via upload

---
+++ server/db/conn.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const URI = process.env.URI;
+
+mongoose.connect(URI).then(()=>
+    console.log('➜ Database connected')
+).catch((error)=> console.log("error" + error.message))
