dde6111be73bac8e8f54ddff22fe1a9d2a267ee7
gurureddy30 <153658709+gurureddy30@users.noreply.github.com>
2023-12-15 11:57:31 +0530

    Add files via upload

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from '../components/header/Navbar'
+import MainComponent from '../components/home/MainComponent'
+import Footer from '../components/footer/Footer'
+import { Routes, Route } from 'react-router-dom'
+import CartRoute from '../routes/CartRoute'
+import Orders from '../routes/Orders'
+import Error from '../routes/Error'
+import GetProductOne from '../routes/products/GetProductOne'
+import '../App.css'
+
+function HomePage() {
+  return (
+    <div className="app_routes">
+        <Navbar/>
+        <div className="main_routes">
+        <Routes>
+            <Route exact path='/' element={<MainComponent/>}></Route>
+            <Route path='/addcart/:id' element={<CartRoute/>}></Route>
+            <Route path='/orders' element={<Orders/>}></Route>
+            <Route path='/getproductsone/:id' element={<GetProductOne/>}></Route>
+            <Route path='*' element={<Error code="404" error="Page not found"/>}></Route>
+            
+        </Routes>
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default HomePage
