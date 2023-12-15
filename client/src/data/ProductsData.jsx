2ce68a17b666da3e6d29e3b27c46400ec4f76172
gurureddy30 <153658709+gurureddy30@users.noreply.github.com>
2023-12-15 14:19:10 +0530

    Add files via upload

---
+++ client/src/data/ProductsData.jsx
@@ -0,0 +1,104 @@
+export const products = [
+    {
+        id: 'product1',
+        url: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/71HeehcVOYS._SX679_.jpg",
+        detailUrl: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/71HeehcVOYS._SX679_.jpg",
+        title: {
+            shortTitle: "Home & Kitchen",
+            longTitle: "Pigeon FAVOURITE Electric Keetle (1.5L, Silver, Black)",
+        },
+        price: {
+            mrp: 1195,
+            cost: 625,
+            discount: "47%",
+        },
+        description: "",
+        discount: "Extra 10% off",
+        tagline: "Deal of the day"
+    },
+    {
+        id: 'product2',
+        url: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61g92a7BkXL._SY741_.jpg",
+        detailUrl: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61g92a7BkXL._SY741_.jpg",
+        title: {
+            shortTitle: "Grocery & Foods",
+            longTitle: "Fresh Produce Monster Energy 350 ml",
+        },
+        price: {
+            mrp: null,
+            cost: 125,
+            discount: "",
+        },
+        description: "",
+        discount: "Extra 10% off",
+        tagline: "Deal of the day"
+    },
+    {
+        id: 'product3',
+        url: "https://images-eu.ssl-images-amazon.com/images/I/81pfI3-4G5L._AC_UL160_SR160,160_.jpg",
+        detailUrl: "https://images-eu.ssl-images-amazon.com/images/I/81pfI3-4G5L._AC_UL160_SR160,160_.jpg",
+        title: {
+            shortTitle: "Faishon",
+            longTitle: "GUESS Analog Black Dial Men's Watch-GW0325G1",
+        },
+        price: {
+            mrp: '13,900',
+            cost: '12,788',
+            discount: "8%",
+        },
+        description: "",
+        discount: "Extra 10% off",
+        tagline: "Deal of the day"
+    },
+    {
+        id: 'product4',
+        url: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/91lbkrbHp1L._SX679_.jpg",
+        detailUrl: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/91lbkrbHp1L._SX679_.jpg",
+        title: {
+            shortTitle: "Home & Kitchen",
+            longTitle: "Amazon Brand - Solimo Medusa Engineered Wood Queen Size Bed Without Storage - (Walnut Finish_Brown)",
+        },
+        price: {
+            mrp: '16,000',
+            cost: '10,499',
+            discount: "34%",
+        },
+        description: "",
+        discount: "Extra 10% off",
+        tagline: "Deal of the day"
+    },
+    {
+        id: 'product5',
+        url: "https://images-eu.ssl-images-amazon.com/images/I/513yW762m7L._AC_UL160_SR160,160_.jpg",
+        detailUrl: "https://images-eu.ssl-images-amazon.com/images/I/513yW762m7L._AC_UL160_SR160,160_.jpg",
+        title: {
+            shortTitle: "Grocery & Foods",
+            longTitle: "Cadbury Dairy Milk Silk, Chocolate Bar, 116g",
+        },
+        price: {
+            mrp: null,
+            cost: 170,
+            discount: "",
+        },
+        description: "",
+        discount: "Extra 10% off",
+        tagline: "Deal of the day"
+    },
+    {
+        id: 'product6',
+        url: "https://images-eu.ssl-images-amazon.com/images/I/51Muj465WmL._AC_UL160_SR160,160_.jpg",
+        detailUrl: "https://images-eu.ssl-images-amazon.com/images/I/51Muj465WmL._AC_UL160_SR160,160_.jpg",
+        title: {
+            shortTitle: "Grocery & Foods",
+            longTitle: "Red Bull Energy Drink Normal, (250ml)",
+        },
+        price: {
+            mrp: null,
+            cost: 165,
+            discount: "",
+        },
+        description: "",
+        discount: "Extra 10% off",
+        tagline: "Deal of the day"
+    },
+]
