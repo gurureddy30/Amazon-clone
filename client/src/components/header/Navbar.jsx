7a3a11b61b697d17afe49e101a5b7f7e1004f8a0
gurureddy30 <153658709+gurureddy30@users.noreply.github.com>
2023-12-15 11:44:03 +0530

    Add files via upload

---
+++ client/src/components/header/Navbar.jsx
@@ -0,0 +1,55 @@
+// Project by NISHANT CHHILLAR
+import React from "react";
+import "./navbar.css";
+import SubNavbar from "./SubNavbar";
+import logo from "../../assets/logo.webp";
+import BorderButton from "../common/BorderButton";
+import IconBorderButton from "../common/IconBorderButton";
+import ImageBorderButton from "../common/ImageBorderButton";
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
+import Cart from "../common/Cart";
+
+function Navbar() {
+    return (
+        <header>
+            <div className="navbar" id="top">
+                <div className="nav-left">
+                    <ImageBorderButton image={logo} name=".in" href="/"/>
+                    <IconBorderButton icon={<FmdGoodOutlinedIcon/>} name="Update location" />
+                </div>
+                <div className="nav-fill">
+                  <div className="nav-search">
+                    <form action="">
+                      <div className="nav-left all-card">
+                        <div className="all-card">
+                          <p>All</p>
+                        </div>
+                      </div>
+                      <div className="nav-fill">
+                        <input type="text" placeholder="Search Amazon.in"/>
+                      </div>
+                      <div className="nav-right sprite">
+                          <span className="nav-sprite-search">
+                            <input type="submit" value="Go"/>
+                          </span>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div className="nav-right">
+                <div className="nav-right accounts">
+                  <BorderButton name="Login" href="/login"/>
+                  <div className="nav-right">
+                  <BorderButton name="Register" href="/register"/>
+                  <BorderButton name="Orders" href="/orders"/></div>
+                  
+                </div><div className="cart"><Cart name="Cart" href="/addcart"/></div></div>
+            </div>
+            <div className="nav-main">
+                <SubNavbar />
+            </div>
+        </header>
+    );
+}
+
+export default Navbar;
---
+++ client/src/components/header/SubNavbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './navbar.css'
+import BorderButton from '../common/BorderButton';
+import IconBorderButton from '../common/IconBorderButton'
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+
+function SubNavbar() {
+  return (
+    <div className="sub-navbar">
+      <div className="sub-left">
+        <IconBorderButton icon={<MenuOutlinedIcon/>} name="All" />
+        </div>
+      <div className="sub-right hide">
+        <BorderButton name="Amazon mini TV" href="/mini-tv"/>
+        <BorderButton name="Sell" href="/sell"/>
+        <BorderButton name="Best Sellers" href="/best-sellers"/>
+        <BorderButton name="Today's Deals" href="/deals"/>
+        <BorderButton name="Customer Service" href="/customer-service"/>
+        <BorderButton name="Electronics" href="/electronics"/>
+        <BorderButton name="Mobiles" href="/mobiles"/>
+        <BorderButton name="Home/ Kitchen" href="/home-kitchen"/>
+        <BorderButton name="Prime" href="/prime"/>
+        <BorderButton name="Faishon" href="/faishon"/>
+        <BorderButton name="Amazon Pay" href="/amazon-pay"/>
+        <BorderButton name="Computers" href="/computers"/>
+      </div>
+    
+    </div>
+  )
+}
+
+export default SubNavbar
